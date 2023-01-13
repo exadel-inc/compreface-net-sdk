39b1e8793b9cc912879fdeee3412ff6344c8c5a6
vldmrklnk <ukalenik@exadel.com>
2023-01-13 17:01:18 +0100

    FaceDetectionServiceTest class was added

---
+++ Exadel.Compreface.AcceptenceTests/Services/FaceDetectionServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Exadel.Compreface.Configuration;
+using static Exadel.Compreface.AcceptenceTests.UrlConstConfig;
+
+namespace Exadel.Compreface.AcceptenceTests.Services
+{
+    public class FaceDetectionServiceTest
+    {
+        private readonly ComprefaceClient comprefaceApiClient;
+        public FaceDetectionServiceTest()
+        {
+            comprefaceApiClient = new ComprefaceClient(new ComprefaceConfiguration(API_KEY_DETECTION_SERVICE, BASE_URL));
+        }
+
+        [Fact]
+        public async Task FaceDetectionAsync_TakesRequestModel_ReturnsProperResponseModel()
+        {
+
+        }
+
+        [Fact]
+        public async Task FaceDetectionBase64Async_TakesRequestModel_ReturnsProperResponseModel()
+        {
+
+        }
+    }
+}
