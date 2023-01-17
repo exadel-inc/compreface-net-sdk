757b1dd5ec69dcb1aa42fc1ea27bb1f7cb2cd998
Asadbek Sindarov <asindarov07@gmail.com>
2023-01-17 14:00:04 +0500

    Fix naming typo

--- Exadel.Compreface.UnitTests/ApiClient/ApiClientTests.cs
+++ Exadel.Compreface.UnitTests/ApiClient/ApiClientTests.cs
@@ -15,8 +15,8 @@ public partial class ApiClientTests : IDisposable
     private const string RequestUrl = "http://site.com";
     private const string ExceptionMessage = "{\"message\":\"Something bad happened!!!\",\"code\":20}";
     private const string SuccessMessage = "{\"message\":\"Everything is good so far!!!\"}";
-    public const int BadRequestStatusCode = 400;
-    public const int SuccessRequestStatusCode = 200;
+    public const int BadResponseStatusCode = 400;
+    public const int SuccessResponseStatusCode = 200;
     
     private static string GetRandomString()
     {
