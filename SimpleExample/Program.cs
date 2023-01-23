6afd05e10990ec415fc69483c42cc238e0b041e6
Asadbek Sindarov <asindarov07@gmail.com>
2023-01-23 22:38:28 +0500

    Hide api credentials in appsettings.json

--- SimpleExample/Program.cs
+++ SimpleExample/Program.cs
@@ -2,7 +2,7 @@
 
 // the rest of the other services will be configured like this
 var faceRecognitionClient = new FaceRecognitionClient(
-    apiKey: "746f45a6-b35e-4087-a79a-a686b3c47fb7",
+    apiKey: "recognition api key here...",
     domain: "http://localhost",
     port: "8000");
 
