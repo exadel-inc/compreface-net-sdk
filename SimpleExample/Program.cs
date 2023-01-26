b2621d2fa628f8cc3d4e6db5f549689b496d3627
alisexadel <99976983+alisexadel@users.noreply.github.com>
2023-01-26 18:09:09 +0100

    updated program file

--- SimpleExample/Program.cs
+++ SimpleExample/Program.cs
@@ -1,87 +1,14 @@
-﻿using System.Threading.Channels;
-using Exadel.Compreface.Clients;
-using Exadel.Compreface.DTOs.ExampleSubjectDTOs.AddExampleSubject;
-using Exadel.Compreface.DTOs.FaceDetectionDTOs.FaceDetection;
-using Exadel.Compreface.DTOs.FaceVerificationDTOs;
-using Exadel.Compreface.DTOs.FaceVerificationDTOs.FaceVerification;
-using Exadel.Compreface.DTOs.FaceVerificationDTOs.FaceVerificationWithBase64;
-using Exadel.Compreface.DTOs.RecognitionDTOs.RecognizeFaceFromImage;
+﻿using Exadel.Compreface.Clients;
 
 // the rest of the other services will be configured like this
 var faceRecognitionClient = new FaceRecognitionClient(
-    apiKey: "3b4d6aaa-252f-41fd-aa6a-052cc6cc1474",
+    apiKey: "recognition api key here...",
     domain: "http://localhost",
     port: "8000");
 
-var faceVerificationClient = new FaceVerificationClient(
-    apiKey: "2283c5cf-4a8b-49d9-9bd7-0a75f49c816b",
-    domain: "http://localhost",
-    port: "8000");
+var subjects = await faceRecognitionClient.SubjectService.GetAllSubject();
 
-//var faceDetectionClient = new FaceDetectionClient(
-//    apiKey: "59e2df37-0781-439d-802a-63329bb7e60a",
-//    domain: "http://localhost",
-//    port: "8000");
-
-var addSubjectExampleRequest = new AddSubjectExampleRequest()
+foreach (var subject in subjects.Subjects)
 {
-    Subject = "Steve",
-    File = "http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcS6j6jazxVpBpf4ZdKMgUeN61C_s4EJPqI7NijX0PDep3SGXQ_bT9ap12h2MWoIolUIpngs3pApkIH-Kzw",
-    //FilePath = "https://raw.githubusercontent.com/exadel-inc/compreface-net-sdk/main/Exadel.Compreface.AcceptenceTests/Resources/Images/brad-pitt_24.jpg",
-};
-var recognizeFaceFromImageRequest = new RecognizeFaceFromImageRequest()
-{
-    FilePath = "https://raw.githubusercontent.com/exadel-inc/compreface-net-sdk/main/Exadel.Compreface.AcceptenceTests/Resources/Images/brad-pitt_24.jpg",
-    DetProbThreshold = 0.85m,
-    FacePlugins = new List<string>()
-            {
-                "age",
-                "gender",
-                "mask",
-                "calculator",
-            },
-    Status = true,
-};
-
-var FaceVerificationExampleRequest = new FaceVerificationRequest()
-{
-    DetProbThreshold = 0.85m,
-    FacePlugins = new List<string>()
-            {
-                "age",
-                "gender",
-                "mask",
-                "calculator",
-            },
-    Status = true,
-    //SourceImageFilePath = @"..\..\..\Users\alis\Downloads\britney-spears-during-the-2000-mtv-video-music-awards-at-news-photo-1636756684.jpg",
-    //TargetImageFilePath = @"..\..\..\Users\alis\Downloads\britney-spears-during-the-2000-mtv-video-music-awards-at-news-photo-1636756684.jpg",
-
-    SourceImageFilePath = "http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcS6j6jazxVpBpf4ZdKMgUeN61C_s4EJPqI7NijX0PDep3SGXQ_bT9ap12h2MWoIolUIpngs3pApkIH-Kzw",
-    TargetImageFilePath = "https://raw.githubusercontent.com/exadel-inc/compreface-net-sdk/main/Exadel.Compreface.AcceptenceTests/Resources/Images/brad-pitt_24.jpg",
-
-};
-var faceDetectionRequest = new FaceDetectionRequest()
-{
-    FilePath = "https://raw.githubusercontent.com/exadel-inc/compreface-net-sdk/main/Exadel.Compreface.AcceptenceTests/Resources/Images/brad-pitt_24.jpg",
-    DetProbThreshold = 0.85m,
-    FacePlugins = new List<string>()
-            {
-                "age",
-                "gender",
-                "mask",
-                "calculator",
-            },
-    Status = true,
-};
-
-//var createdSubjectExample = await faceRecognitionClient.RecognitionService.RecognizeFaceFromImage(recognizeFaceFromImageRequest, isFileInTheRemoteServer: true);
-var createdSubjectExample = await faceRecognitionClient.SubjectExampleService.AddSubjectExampleAsync(addSubjectExampleRequest, isFileInTheRemoteServer: true);
-//var createdSubjectExample = await faceVerificationClient.FaceVerificationService.VerifyBase64ImageAsync(faceVerificationWithBase64Request);
-//var createdSubjectExample = await faceVerificationClient.FaceVerificationService.VerifyImageAsync(FaceVerificationExampleRequest, isFileInTheRemoteServer: true);
-//var createdSubjectExample = await faceDetectionClient.FaceDetectionService.FaceDetectionAsync(faceDetectionRequest, isFileInTheRemoteServer: true);
-
-
-
-
-Console.WriteLine("");
+    Console.WriteLine(subject);
+}
