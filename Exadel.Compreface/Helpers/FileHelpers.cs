4263f7d70228016f1c91cf619167ce4113d8170f
alisexadel <99976983+alisexadel@users.noreply.github.com>
2023-02-01 15:57:06 +0100

    FileHelpers

--- Exadel.Compreface/Helpers/FileHelpers.cs
+++ Exadel.Compreface/Helpers/FileHelpers.cs
@@ -4,10 +4,10 @@ public class FileHelpers
 {
     public static string GenerateFileName(string filePath)
     {
-        //if (!File.Exists(filePath))
-        //{
-        //    throw new FileNotFoundException(message: $"file does not exist in path : {filePath}!!!");
-        //}
+        if (!File.Exists(filePath))
+        {
+            throw new FileNotFoundException(message: $"file does not exist in path : {filePath}!!!");
+        }
 
         var fileExtension = Path.GetExtension(filePath);
 
