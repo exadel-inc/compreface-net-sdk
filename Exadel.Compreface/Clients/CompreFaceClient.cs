18ced6e8ec84bdfffd7c86e838743bfac6938eef
alisexadel <99976983+alisexadel@users.noreply.github.com>
2023-02-06 10:36:54 +0100

    updated config

--- Exadel.Compreface/Clients/CompreFaceClient.cs
+++ Exadel.Compreface/Clients/CompreFaceClient.cs
@@ -16,10 +16,10 @@ public class CompreFaceClient
     public CompreFaceClient(IComprefaceConfiguration configuration)
         : this(configuration.Domain, configuration.Port) { }
 
-    public CompreFaceClient(string domain, string port)
+    public CompreFaceClient(string? domain, string? port)
     {
-        _domain = domain;
-        _port = port;
+        _domain = domain ?? throw new ArgumentNullException($"{nameof(domain)} cannot be null!");
+        _port = port ?? throw new ArgumentNullException($"{nameof(port)} cannot be null!");
     }
 
     public T GetService<T>(string apiKey) where T : AbstractBaseService
