4d57e6b2d42d0d41b516ed9ba46a0b2b1a081aaf
vldmrklnk <ukalenik@exadel.com>
2023-02-06 23:44:17 +0100

    Description of attribute.

--- Exadel.Compreface/Services/Attributes/CompreFaceServiceAttribute.cs
+++ Exadel.Compreface/Services/Attributes/CompreFaceServiceAttribute.cs
@@ -1,5 +1,8 @@
 ﻿namespace Exadel.Compreface.Services.Attributes
 {
+    /// <summary>
+    /// Tag for CompreFace services.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
     public class CompreFaceService : Attribute
     {
