3c48d1748f0abf6d0d5bfe40e6d39a975c603dd3
vldmrklnk <ukalenik@exadel.com>
2023-03-21 22:24:51 +0100

    13

--- Exadel.Compreface.AcceptenceTests/Services/VerificationServiceTest.cs
+++ Exadel.Compreface.AcceptenceTests/Services/VerificationServiceTest.cs
@@ -62,15 +62,15 @@ namespace Exadel.Compreface.AcceptenceTests.Services
             };
         }
 
-        //[Fact]
-        //public async Task VerifyAsync_TakesRequestModel_ReturnsProperResponseModel()
-        //{
-        //    // Act
-        //    var response = await _faceVerificationService.VerifyAsync(_faceVerificationRequest);
+        [Fact]
+        public async Task VerifyAsync_TakesRequestModel_ReturnsProperResponseModel()
+        {
+            // Act
+            var response = await _faceVerificationService.VerifyAsync(_faceVerificationRequest);
 
-        //    // Assert
-        //    Assert.IsType<FaceVerificationResponse>(response);
-        //}
+            // Assert
+            Assert.IsType<FaceVerificationResponse>(response);
+        }
 
         //[Fact]
         //public async Task VerifyAsync_TakesRequestModel_ReturnsNotNull()
@@ -120,15 +120,15 @@ namespace Exadel.Compreface.AcceptenceTests.Services
         //    await Assert.ThrowsAsync<ServiceException>(func);
         //}
 
-        [Fact]
-        public async Task VerifyBase64Async_TakesRequestModel_ReturnsProperResponseModel()
-        {
-            // Act
-            var response = await _faceVerificationService.VerifyAsync(_faceVerificationBase64Request);
+        //[Fact]
+        //public async Task VerifyBase64Async_TakesRequestModel_ReturnsProperResponseModel()
+        //{
+        //    // Act
+        //    var response = await _faceVerificationService.VerifyAsync(_faceVerificationBase64Request);
 
-            // Assert
-            Assert.IsType<FaceVerificationResponse>(response);
-        }
+        //    // Assert
+        //    Assert.IsType<FaceVerificationResponse>(response);
+        //}
 
         //    [Fact]
         //    public async Task VerifyBase64Async_TakesRequestModel_ReturnsNotNull()
