e9e742a4419331f3878d2259dda155b68bc3414f
vldmrklnk <ukalenik@exadel.com>
2023-03-25 00:45:23 +0100

    add mvc project

---
+++ Exadel.Compreface.ExampleApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Exadel.Compreface.ExampleApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
